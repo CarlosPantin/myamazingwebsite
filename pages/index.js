c0a7cb4d657579c8f3788dadb6d83e878aea491b
CarlosPantin <109794483+CarlosPantin@users.noreply.github.com>
2022-11-08 19:34:56 +0200

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-\import Head from 'next/head'
+import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillLinkedin} from 'react-icons/ai'
 import {FiArrowRight,FiArrowLeft} from "react-icons/fi";
